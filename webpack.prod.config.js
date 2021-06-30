fbe82a1ea5ae8b2c7129c418232bd0d2c4aedda3
NastyaZhilko <nasten4ik2010@gmail.com>
2021-06-30 15:35:46 +0300

    gh-pages

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,9 @@
-const path = require('path')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devtool: 'source-map',
     mode: "development",
     entry: ["@babel/polyfill", "./src/index.jsx"],
     output: {
@@ -49,4 +50,4 @@ module.exports = {
             }
         ]
     }
-}
+};
